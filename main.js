272869d9f1731a597660370052a4edfe2bea7be9
Sergio Gonzalez <s.gonzalez@codeur.online>
2022-08-02 13:46:41 +0200

    ajout node js

---
+++ main.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+
+    host: "localhost",
+ 
+    user: "root",
+ 
+    password: "root"
+ 
+  });
+
+  db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connecté à la base de données MySQL!");
+  });
+
+  
